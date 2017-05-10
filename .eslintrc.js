04f26d97d04a2d25ae5b9d7eee3ffe76292d5ff3
David Martin <davidjmartin01@gmail.com>
2017-05-09 19:58:45 -0700

    initialized app, configured linter

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "extends": [
+        "airbnb",
+        "plugin:react-native/all"
+    ],
+    "plugins": [
+        "react",
+        "react-native",
+        "jsx-a11y",
+        "import"
+    ]
+};
---
+++ README.md
@@ -0,0 +1,1 @@
+This is a simple messaging application made with React Native. It was bootstrapped with [Create React Native App](https://github.com/react-community/create-react-native-app).
