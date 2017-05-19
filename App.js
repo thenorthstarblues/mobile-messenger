0854c44779a051a150f74fc4cb1c3f31f7190cac
David Martin <davidjmartin01@gmail.com>
2017-05-19 12:08:57 -0700

    updated expo and react native versions

--- App.js
+++ App.js
@@ -1,14 +1,16 @@
-import { View } from 'react-native';
+import { View, AppRegistry } from 'react-native';
 import React, { Component } from 'react';
 import Login from './src/components/login';
 
 
-export default class App extends Component {
-  render() {
+export default class MobileMessenger extends Component {
+  render () {
     return (
       <View style={{ flex: 1 }}>
         <Login />
       </View>
-    );
+    )
   }
 }
+
+AppRegistry.registerComponent('MobileMessenger', () => MobileMessenger)
