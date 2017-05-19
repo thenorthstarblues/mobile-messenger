7628592232aaf97e592a4932cf751316928cb287
David Martin <davidjmartin01@gmail.com>
2017-05-19 12:34:56 -0700

    removed RN linter plugin

--- App.js
+++ App.js
@@ -13,4 +13,4 @@ export default class MobileMessenger extends Component {
   }
 }
 
-AppRegistry.registerComponent('MobileMessenger', () => MobileMessenger)
+AppRegistry.registerComponent('main', () => MobileMessenger)
