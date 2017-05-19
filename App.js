9ff2be017a7da0acb71b1fd0a185c5390ac480c5
David Martin <davidjmartin01@gmail.com>
2017-05-19 13:25:53 -0700

    added fields to login page and styled

--- App.js
+++ App.js
@@ -1,16 +1,23 @@
-import { View, AppRegistry } from 'react-native';
+import { View, AppRegistry, StyleSheet } from 'react-native';
 import React, { Component } from 'react';
 import Login from './src/components/login';
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
 
 export default class MobileMessenger extends Component {
-  render () {
+  render() {
     return (
-      <View style={{ flex: 1 }}>
+      <View style={styles.container}>
         <Login />
       </View>
-    )
+    );
   }
 }
 
-AppRegistry.registerComponent('main', () => MobileMessenger)
+
+
+AppRegistry.registerComponent('main', () => MobileMessenger);
--- src/components/login.js
+++ src/components/login.js
@@ -1,28 +1,88 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableHighlight,
+  StyleSheet,
+} from 'react-native';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'stretch',
-    backgroundColor: '#6E5BAA',
-  },
-});
-
-class Login extends Component {
+export default class Login extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      username: '',
+    };
   }
 
-  render() {
+  onPress = (e) => {
+    console.log(this.state.username);
+  }
+
+  render(){
     return (
       <View style={styles.container}>
-        <Text style={{ color: 'white' }}>Login Screen?</Text>
+        <View style={styles.loginContainer}>
+          <TextInput
+            style={styles.input}
+            value={this.state.username}
+            onChangeText={(text) => this.setState({username: text})}
+            placeholder={'Enter User Nickname'}
+            maxLength={12}
+            multiline={false}
+            />
+          <TouchableHighlight
+            style={styles.button}
+            underlayColor={'#328FE6'}
+            onPress={this.onPress}
+            >
+            <Text style={styles.label}>LOGIN</Text>
+          </TouchableHighlight>
+        </View>
       </View>
     );
   }
 }
 
-export default Login;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#6E5BAA',
+  },
+  loginContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  input: {
+    width: 250,
+    color: '#555555',
+    padding: 10,
+    height: 50,
+    borderColor: '#32C5E6',
+    borderWidth: 1,
+    borderRadius: 4,
+    alignSelf: 'center',
+    backgroundColor: '#ffffff',
+  },
+  button: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderWidth: 1,
+    borderRadius: 5,
+    borderColor: '#328FE6',
+    padding: 10,
+    marginTop: 10,
+    backgroundColor: '#32c5e6',
+  },
+  label: {
+    width: 230,
+    alignSelf: 'center',
+    textAlign: 'center',
+    fontSize: 20,
+    fontWeight: '600',
+    color: '#ffffff',
+  },
+});
