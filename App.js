20bfc5d751e0f1c9de91ca459d808fc2543d3c21
David Martin <davidjmartin01@gmail.com>
2017-05-18 22:37:40 -0700

    added login component, first screen rendering

--- App.js
+++ App.js
@@ -1,23 +1,14 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
+import React, { Component } from 'react';
+import Login from './src/components/login';
 
-export default class App extends React.Component {
+
+export default class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Making changes</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+      <View style={{ flex: 1 }}>
+        <Login />
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/components/login.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'stretch',
+    backgroundColor: '#6E5BAA',
+  },
+});
+
+class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={{ color: 'white' }}>Login Screen?</Text>
+      </View>
+    );
+  }
+}
+
+export default Login;
