69288938d1cfca9df29f4ea4856c4081662fa526
David Martin <davidjmartin01@gmail.com>
2017-05-21 21:10:33 -0700

    changed login screen background color

--- src/components/login.js
+++ src/components/login.js
@@ -47,7 +47,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#8dffca',
+    backgroundColor: '#ffffff',
   },
   input: {
     width: 250,
