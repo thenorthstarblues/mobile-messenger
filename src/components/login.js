8d543068aa8b68bfecc43b2e1aa388cd3a2ff049
David Martin <davidjmartin01@gmail.com>
2017-05-27 17:55:59 -0700

    switched text input to material

--- src/components/login.js
+++ src/components/login.js
@@ -1,13 +1,6 @@
 import React, { Component } from 'react';
-import {
-  View,
-  Text,
-  TextInput,
-  TouchableHighlight,
-  StyleSheet,
-  Image,
-} from 'react-native';
-import { Button, Icon } from 'react-native-elements';
+import { View, StyleSheet } from 'react-native';
+import { Button, Icon, FormInput } from 'react-native-elements';
 
 export default class Login extends Component {
   constructor(props) {
@@ -32,13 +25,11 @@ export default class Login extends Component {
           />
         </View>
         <View style={styles.loginBox}>
-          <TextInput
+          <FormInput
             style={styles.input}
             value={this.state.username}
             onChangeText={(text) => this.setState({username: text})}
-            placeholder={'Enter User Nickname'}
-            maxLength={12}
-            multiline={false}
+            placeholder={'Enter Username'}
             />
           <Button
             buttonStyle={styles.button}
@@ -68,23 +59,15 @@ const styles = StyleSheet.create({
   },
   input: {
     width: 250,
-    color: '#555555',
     padding: 10,
     height: 50,
-    borderWidth: 1,
-    borderRadius: 4,
-    borderColor: 'rgba(0, 0, 0, 0.3)',
-    alignSelf: 'center',
-    backgroundColor: '#ffffff',
   },
   button: {
-    borderRadius: 5,
     padding: 10,
     marginTop: 10,
   },
   label: {
     textAlign: 'center',
-    width: 230,
     fontSize: 20,
     fontWeight: '600',
   },
